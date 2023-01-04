a73303bd2ac68184da4ba11fe28668914d5b7d95
jhuanfigueroa <figueroatrejojuan1h@gmail.com>
2023-01-04 03:30:08 -0600

    make it better

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('api/users/create',views.createUser().as_view(),name='registrar'),
     path('api/users',views.getUsers().as_view()),
     path('file',views.FileUploadView.as_view(),name='file'),
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- signatures/settings.py
+++ signatures/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from unipath import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,11 +140,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+"""STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT=BASE_DIR.child('media')
+MEDIA_ROOT=BASE_DIR.child('media')"""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_TMP=os.path.join(BASE_DIR,'static')
+STATIC_URL='/static/'
+os.makedirs(STATIC_TMP,exist_ok=True)
+os.makedirs(STATIC_ROOT,exist_ok=True)
+
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'static'),
+)
+
+
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
